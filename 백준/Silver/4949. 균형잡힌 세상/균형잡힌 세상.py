708616f21fdea39a81627f85c98716b765d6865b
Khan <123632194+Wonseoop@users.noreply.github.com>
2025-01-08 22:04:21 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 76 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -3,25 +3,22 @@ from collections import deque
 
 def CheckLine(line):
     dq = deque()
-    
     for w in line:
-        if w == "(" or w == "[":
+        if w == '[' or w == '(':
             dq.append(w)
-        elif w == ")" or w == "]":
-            if not dq:  # 스택이 비어 있으면 짝이 맞지 않음
+        elif w == ']' or w == ')':
+            if not dq:
                 return False
             top = dq.pop()
-            if (w == ")" and top != "(") or (w == "]" and top != "["):
+            if (w == ']' and top != '[') or (w == ')' and top != '('):
                 return False
-                
-    return len(dq) == 0  # 스택이 비어 있으면 모든 괄호가 짝이 맞음
+    return len(dq) == 0
 
 def endcheck(a):
     return len(a) == 1 and a[0] == '.'
 
-flag = True
-while flag:
-    line = sys.stdin.readline().rstrip("\n")  
+while True:
+    line = sys.stdin.readline().rstrip("\n")
     if endcheck(line):
         break
     if CheckLine(line):

f24d13af7cf0242e2cee71213c28caea20fec16f
Khan <123632194+Wonseoop@users.noreply.github.com>
2025-01-29 11:37:42 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.cc
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.cc
@@ -1,31 +1,40 @@
+
+
 #include <iostream>
+#include <deque>
+#include <vector>
 #include <set>
+
 
 using namespace std;
 
-void backjoon() {
-    set<string> s;
-    string s1, s2;
-    int N;
+void backjoon()
+{
+    //ios::sync_with_stdio(false);    // cin, cout 속도 개선
+    //cin.tie(nullptr);               // cin cout 순서 무시
 
+    set<string> s;
+    string s1,s2;
+    int N;
     cin >> N;
 
-    s.insert("ChongChong"); // 시작 인물 "ChongChong"을 미리 추가
+    s.insert("ChongChong");
 
     while (N--) {
-        cin >> s1 >> s2;
-
-        // s1 또는 s2 중 하나라도 "춤을 추는 사람 목록(s)"에 있다면, 둘 다 감염
+        cin >> s1>> s2;
         if (s.count(s1) || s.count(s2)) {
             s.insert(s1);
             s.insert(s2);
         }
     }
 
-    cout << s.size(); // 전체 "춤을 추는 사람"의 수 출력
+    cout << s.size();
 }
 
-int main() {
+int main()
+{
     backjoon();
+
+   
     return 0;
 }

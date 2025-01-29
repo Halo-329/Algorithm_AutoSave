fdb66c987e644814530eae5515ee64ec8cd362d3
Khan <123632194+Wonseoop@users.noreply.github.com>
2025-01-29 21:29:58 +0900

    [Silver III] Title: 통계학, Time: 200 ms, Memory: 4248 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -1,55 +1,110 @@
 #include <iostream>
+#include <deque>
 #include <vector>
 #include <numeric>
 #include <map>
 #include <algorithm>
 #include <cmath>
+#include <limits.h>
+
 
 using namespace std;
 
-void backjoon() {
+void PrintV(vector<int> &v)
+{
+    for (int i : v)
+    {
+        cout << i <<endl;
+    }
+}
+void backjoon()
+{
     int N;
+    int num;
+
     cin >> N;
 
     vector<int> nums(N);
-    for (int &i : nums) {
+
+    for (int &i : nums)
+    {
         cin >> i;
     }
 
-    // 1 : 산술평균 (반올림)
+    // 1 : 산술평균
     
-    int mean = round(accumulate(nums.begin(), nums.end(), 0.0) / N);
+    int mean = round(double(accumulate(nums.begin(), nums.end(), 0)) / N);
+    if(mean>=1){
+        mean = ceil(mean);
+    }
+    else if( mean<=-1){
+        mean = floor(mean);
+    }
+    else if (-1<mean &&mean < 1){
+        mean=0;
+    }
     cout << mean << "\n";
 
     // 2 : 중앙값
     sort(nums.begin(), nums.end());
-    cout << nums[N / 2] << "\n";
+
+     cout<<nums.at(N/2)<<"\n";
+    // PrintV(nums);
 
     // 3 : 최빈값
     map<int, int> m;
-    for (int i : nums) {
+    for (int i : nums)
+    {
         m[i]++;
     }
 
-    vector<pair<int, int>> freq(m.begin(), m.end());
-    sort(freq.begin(), freq.end(), [](pair<int, int> a, pair<int, int> b) {
-        if (a.second == b.second) return a.first < b.first;
-        return a.second > b.second;
-    });
+    // for (auto k : m){
+    //     cout<<"("<<k.first<<", "<<k.second<<"), ";
+    // }
+    int max_cnt = 0;
 
-    if (freq.size() == 1 || freq[0].second != freq[1].second) {
-        cout << freq[0].first << "\n";
-    } else {
-        cout << freq[1].first << "\n";
+    vector<int> a;
+    for (auto pair : m)
+    {
+        if (max_cnt < pair.second)
+        {
+            max_cnt = pair.second;
+            a.clear();
+            a.push_back(pair.first);
+        }
+        else if (pair.second == max_cnt)
+        {
+            a.push_back(pair.first);
+        }
+    }
+    //cout<<"a. size : "<<a.size()<<endl;
+    if(a.size()==1){
+        cout<<a[0]<<"\n";
+    }
+    else{
+        sort(a.begin(),a.end());
+    cout<<a[1]<<endl;
     }
 
-    // 4 : 범위 계산
-    int maxVal = *max_element(nums.begin(), nums.end());
-    int minVal = *min_element(nums.begin(), nums.end());
-    cout << (maxVal - minVal) << "\n";
+    // 4 : 범위
+    int max = *max_element(nums.begin(), nums.end());
+    int min = *min_element(nums.begin(), nums.end());
+    cout<<max-min;
+    
 }
 
-int main() {
+void test()
+{
+    float a = (float)9/(float)5;
+
+    cout << (a);
+}
+
+int main()
+{
+
+    // bollon();
     backjoon();
+    //test();
     return 0;
 }

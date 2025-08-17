00ea8eb27a92ab4cf8f283cc5776c5f3a2a2d350
halo <123632194+Halo-329@users.noreply.github.com>
2025-08-17 18:22:57 +0900

    [Bronze I] Title: 평균, Time: 308 ms, Memory: 26300 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double max=0;
+        double[] arr = new double[n];
+
+        for (int i=0; i<n; i++){
+            arr[i]=sc.nextDouble();
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+                arr[i]=arr[i]/max*100;
+
+
+        }
+
+        double sum=0;
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+        }
+
+        System.out.println(sum/n);
+    }
+}

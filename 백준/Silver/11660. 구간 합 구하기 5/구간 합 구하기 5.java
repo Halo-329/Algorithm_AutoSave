6eb13fe70cf4972e22fcd929cc4f3853e05b8370
halo <123632194+Halo-329@users.noreply.github.com>
2025-08-19 10:13:10 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2720 ms, Memory: 328920 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int N, M;
+        int[][] A, DP;
+        Scanner sc = new Scanner(System.in);
+
+        N=sc.nextInt();
+        M=sc.nextInt();
+        A = new int[N+1][N+1];
+        DP = new int[N+1][N+1];
+
+        for (int i=1; i<=N; i++){
+            for (int j=1; j<=N; j++){
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=1; i<=N; i++){
+            for (int j=1; j<=N; j++){
+                DP[i][j] =DP[i-1][j] + DP[i][j-1] - DP[i-1][j-1] + A[i][j];
+            }
+        }
+
+        for (int i=0; i<M; i++){
+            int x1,x2,y1,y2;
+            x1=sc.nextInt();
+            y1=sc.nextInt();
+            x2=sc.nextInt();
+            y2=sc.nextInt();
+
+
+            int ans = DP[x2][y2] - DP[x2][y1-1] - DP[x1-1][y2] + DP[x1-1][y1-1];
+            System.out.println(ans);
+        }
+
+
+    }
+}

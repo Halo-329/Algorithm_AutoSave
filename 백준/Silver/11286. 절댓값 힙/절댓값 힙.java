a20357015292f4d3af1605f58f6ed89b9040eb5d
halo <123632194+Halo-329@users.noreply.github.com>
2025-08-30 15:09:44 +0900

    [Silver I] Title: 절댓값 힙, Time: 292 ms, Memory: 25528 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,40 +1,31 @@
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        {
-            PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) ->{
-                if(Math.abs(a)==Math.abs(b)){
-                    return a-b;
-                }
-                return Math.abs(a)- Math.abs(b);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            int aa = Math.abs(a), bb = Math.abs(b);
+            if (aa == bb) return a - b;     // 절댓값 같으면 실제 값이 작은 것 우선
+            return aa - bb;                  // 절댓값이 작은 것 우선
+        });
 
-            });
+        int N = Integer.parseInt(br.readLine());
 
-
-            Scanner sc = new Scanner(System.in);
-            int N = sc.nextInt();
-
-            for (int i=0; i<N; i++){
-                int n = sc.nextInt();
-
-                if(n==0){
-                    if(pq.isEmpty()){
-                        System.out.println(0);
-                    }
-                    else{
-                        System.out.println(pq.poll());
-                    }
-                }
-                else{
-                    pq.add(n);
-                }
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                if (pq.isEmpty()) sb.append('0').append('\n');
+                else sb.append(pq.poll()).append('\n');
+            } else {
+                pq.add(n);
             }
-
-
-
         }
+
+        bw.write(sb.toString());
+        bw.flush();
     }
 }

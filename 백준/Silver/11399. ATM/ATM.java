2983c7673ba370361e2a6427abb5b822735e8690
halo <123632194+Halo-329@users.noreply.github.com>
2025-09-07 13:24:48 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 15708 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,29 +1,51 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
 
-        int res = 0;
-        int N = sc.nextInt();
-        Integer[] arr = new Integer[N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter (System.out));
+        StringTokenizer st;
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        int[] sum = new int[N];
+
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, (a, b) -> a - b);
+        for (int i=1; i<N; i++){
+            for (int j=0; j<=i-1; j++){
+                if(arr[j]>=arr[i]){
+                    int tmp=arr[i];
+                    for(int k=i; k>=j+1; k--){
+                        arr[k]=arr[k-1];
+                    }
+                    arr[j]=tmp;
+                    break;
 
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j <= i; j++) {
-                res += arr[j];
-
+                }
             }
         }
 
-        System.out.println(res);
+        sum[0]=arr[0];
+
+        for(int i=1; i<N; i++){
+            sum[i]=arr[i]+sum[i-1];
+        }
+
+        bw.write(Arrays.stream(sum).sum()+"");
+        bw.flush();
+        bw.close();
+
+
     }
 }
